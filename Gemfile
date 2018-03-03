fa92114396cefba05e84773e176e227a5f939ffc
Liz House <lizehouse@gmail.com>
2018-03-02 23:46:20 -0700

    acccout and user set up with playlists

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jbuilder', '~> 2.5'
+gem 'semantic-ui-sass'
+gem 'devise'
 
 group :development, :test do
   gem 'pry'
---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,50 @@
+class AccountsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_account, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @accounts = current_user.accounts
+  end
+
+  def show
+  end
+
+  def new
+    @account = Account.new
+  end
+
+  def edit
+  end
+
+  def create
+    @account = current_user.accounts.new(account_params)
+    if @account.save
+      redirect_to accounts_path
+    else
+      render :new
+    end
+  end
+
+  def udpate
+    if @account.update(account_params)
+      redirect_to accounts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @account.destroy
+  end
+
+  private 
+    def set_account
+      #don't just users Account.find(params[:id]) or you would be able to view
+      #others accounts
+      @account = current_user.accounts.find(params[:id])
+    end
+
+    def account_params
+      params.require(:account).permit(:name, :balance)
+    end
+end
